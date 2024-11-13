c49344494cd92b71a9b5fde151ef6e56c8665646
既安特翁 <1316151698@qq.com>
2024-11-13 23:00:53 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="app">
+        <h1>你好啊</h1>
+        <Person/>
+    </div>
+</template>
+
+<script lang="ts">
+    import Person from "./components/person.vue"  // 前边的Person是person.vue文件中的export default的name，是我们自己给组件起的名。
+    export default{
+        name:'App',
+        components:{Person} // 注册组件，注册之后就可以使用了Person指的是组件名字
+    }
+</script>
+   
+<style>
+    .app {
+        background-color: #ddd;
+        box-shadow: 0 0 10px ;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="person">
+        <h1>你好啊</h1>
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click="showTel">查看联系方式</button>
+
+        <!-- <h2>电话：{{ tel }}</h2> -->
+
+        </div>
+</template>
+
+<script lang="ts">
+    export default{
+        name:'Person',
+        data(){
+            return{
+                name:"张三",
+                age:"18",
+                tel:"18888888888"
+            }
+        },
+        methods:{
+            showTel(){
+                alert(this.tel)
+            }
+        }
+    }
+</script>
+   
+<style>
+    .person{
+        background-color: skyblue;
+        box-shadow: 0 0 10px ;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>

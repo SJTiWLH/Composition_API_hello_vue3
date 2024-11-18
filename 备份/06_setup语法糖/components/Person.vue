509028654eefc37bce207faf584cd50cd4f72351
既安特翁 <1316151698@qq.com>
2024-11-18 21:55:00 +0800

    响应式

---
+++ 备份/06_setup语法糖/components/Person.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="person">
+        <h2>{{name}}</h2>
+        <h1>你好啊</h1>
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="showTel">查看联系方式</button>
+        </div>
+</template>
+
+<!-- <script lang="ts">
+    export default{
+        name:'Person',   //为了给组件起名字。
+    }
+</script> -->
+
+<script lang="ts" setup>
+    // let name = 'li-san'
+    let name = '李四'
+    let age = 18
+    let tel = '123456789'
+
+    function changeName(){
+        name = 'zhang-san' //赋值操作，不是响应式
+        console.log(name) //确实修改了name，但不是响应式所以不展示
+    }
+    function changeAge(){
+        age += 1
+        console.log(age)
+    }
+    function showTel(){
+        alert(tel)
+        console.log(tel)
+    }
+</script>
+
+<style>
+    .person{
+        background-color: skyblue;
+        box-shadow: 0 0 10px ;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    button{
+        margin: 5px
+    }
+</style>
---
+++ 备份/07_ref基本类型数据的响应式/components/Person.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="person">
+        <h2>{{name}}</h2>
+        <h1>你好啊</h1>
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="showTel">查看联系方式</button>
+        </div>
+</template>
+
+
+<script lang="ts" setup name="Person">
+    import { ref } from 'vue' // ref是对象类型的响应式，引入ref后，只需要把需要响应的数据用括号包住前面写上ref即可
+
+    let name = ref('李四')  // ref是对象类型的响应式
+    let age = ref(18)
+    let tel = '123456789'
+    console.log(name)
+    console.log(age)
+    console.log(tel)
+    
+
+    function changeName(){
+        name.value = 'zhang-san' // name是响应式数据，所以可以修改，但是需要给name加上.value
+        console.log(name) 
+    }
+    function changeAge(){
+        age.value += 1
+        console.log(age)
+    }
+    function showTel(){
+        alert(tel)
+        console.log(tel)
+    }
+</script>
+
+<style>
+    .person{
+        background-color: skyblue;
+        box-shadow: 0 0 10px ;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    button{
+        margin: 5px
+    }
+</style>
